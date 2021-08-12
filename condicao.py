8e583a98f8106bd6f56d9f0440af6ac1db0cfbef
TioToninho <antonioviniciussm@outlook.com>
2021-08-12 16:04:17 -0300

    dia 1 atividades uri

---
+++ condicao.py
@@ -0,0 +1,9 @@
+num1 = int(input(""))
+num2 = int(input(""))
+
+if num2 > num1:
+    print("O segundo numero é maior que o primeiro")
+elif num1 > num2:
+    print("O primeiro numero é maior que o segundo")
+else:
+    print("os numeros são iguais")
