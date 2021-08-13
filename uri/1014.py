1dcbf0b2284e546f44fdacb09acf2f6ff1834ec6
Cadumvk <cadubr69@gmail.com>
2021-08-13 17:35:04 -0300

    1015 não terminada, duvida sobre bibliotecas

---
+++ uri/1014.py
@@ -0,0 +1,5 @@
+X = int(input(""))
+Y = float(input(""))
+con_med = X / Y
+
+print("{:.3f} km/l".format(con_med))
---
+++ uri/1015.py
@@ -0,0 +1,8 @@
+p1 = (input().split(" "))
+x1 = float(p1[0])
+y1 = float(p1[1])
+
+p2 = (input().split(" "))
+x2 = float(p2[0])
+y2 = float(p2[1])
+
